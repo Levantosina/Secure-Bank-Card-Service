a5555b7e97ee12afed8574060921581d145114c6
levan <levantosina1992@gmail.com>
2025-05-09 15:53:54 +0300

    create SwaggerConfig

---
+++ src/main/java/io/github/levantosina/bankcardmanagement/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package io.github.levantosina.bankcardmanagement;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Movie Reservation System API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@EnableWebMvc
+public class SwaggerConfig {
+}
