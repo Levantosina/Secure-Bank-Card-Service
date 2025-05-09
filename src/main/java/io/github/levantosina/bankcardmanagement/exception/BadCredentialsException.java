486b7bc1c301ef68a2ab1b835a439e302eb23e60
levan <levantosina1992@gmail.com>
2025-05-09 15:53:44 +0300

    create BadCredentialsException

---
+++ src/main/java/io/github/levantosina/bankcardmanagement/exception/BadCredentialsException.java
@@ -0,0 +1,7 @@
+package io.github.levantosina.bankcardmanagement.exception;
+
+public class BadCredentialsException extends RuntimeException {
+    public BadCredentialsException(String message) {
+        super(message);
+    }
+}
