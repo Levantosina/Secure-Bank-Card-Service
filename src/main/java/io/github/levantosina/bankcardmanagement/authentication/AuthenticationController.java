5bbd0303c3218777d98fa3a802f1b5cd1de18517
levan <levantosina1992@gmail.com>
2025-05-09 15:50:15 +0300

    fix login method

--- src/main/java/io/github/levantosina/bankcardmanagement/authentication/AuthenticationController.java
+++ src/main/java/io/github/levantosina/bankcardmanagement/authentication/AuthenticationController.java
@@ -1,8 +1,10 @@
 package io.github.levantosina.bankcardmanagement.authentication;
 
+import io.github.levantosina.bankcardmanagement.exception.BadCredentialsException;
 import io.github.levantosina.bankcardmanagement.request.UserRegistrationRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/auth")
 @AllArgsConstructor
+@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -26,10 +29,12 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
         try {
-            AuthenticationResponse response = authenticationService.login(authenticationRequest);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new AuthenticationResponse("Invalid credentials"));
+            AuthenticationResponse authenticationResponse = authenticationService.login(authenticationRequest);
+            return ResponseEntity.ok(authenticationResponse);
+        } catch (BadCredentialsException ex) {
+            log.warn("Authentication failed for user {}: {}", authenticationRequest.userName(), ex.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new AuthenticationResponse("Invalid username or password"));
         }
     }
 }
