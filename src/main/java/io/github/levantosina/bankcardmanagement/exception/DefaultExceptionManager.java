e1fcc6e925bd9b2bd1728f3735ec822577b5d9fa
levan <levantosina1992@gmail.com>
2025-05-09 15:51:19 +0300

    add handleBadCredentialsException exception

--- src/main/java/io/github/levantosina/bankcardmanagement/exception/DefaultExceptionManager.java
+++ src/main/java/io/github/levantosina/bankcardmanagement/exception/DefaultExceptionManager.java
@@ -5,20 +5,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class DefaultExceptionManager {
 
     @ExceptionHandler(CardStatusException.class)
-    public ResponseEntity<String> handleCardBlockedException(CardStatusException ex) {
+    public ResponseEntity<?> handleCardBlockedException(CardStatusException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(RequestValidationException.class)
-    public ResponseEntity<String> handleRequestValidationException(RequestValidationException ex) {
+    public ResponseEntity<?> handleRequestValidationException(RequestValidationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException ex) {
+        return  ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("error", ex.getMessage()));
     }
 }
