5029e39f79576a1042fc1b3513ad9853ea320e75
levan <levantosina1992@gmail.com>
2025-05-09 15:50:55 +0300

    add  findByUserUserId &  findByUserUserIdAndCardHolderNameContaining

--- src/main/java/io/github/levantosina/bankcardmanagement/repository/CardRepository.java
+++ src/main/java/io/github/levantosina/bankcardmanagement/repository/CardRepository.java
@@ -1,12 +1,16 @@
 package io.github.levantosina.bankcardmanagement.repository;
 
 import io.github.levantosina.bankcardmanagement.model.CardEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 public interface CardRepository extends JpaRepository<CardEntity, Long> {
     void deleteCardByCardId(Long id);
     boolean existsCardByCardId(Long id);
-    List<CardEntity> findAllByUserUserId(Long userId);
+    Page<CardEntity> findByUserUserId(Long userId, Pageable pageable);
+    Page<CardEntity> findByUserUserIdAndCardHolderNameContaining(Long userId, String cardHolderName, Pageable pageable);
+
 }
