37b22ebbc70d4368ba0c04fc3f98473eec92cb1a
levan <levantosina1992@gmail.com>
2025-05-11 10:59:50 +0300

    create AdminCardRegistrationRequest

--- src/main/java/io/github/levantosina/bankcardmanagement/request/AdminCardRegistrationRequest.java
+++ src/main/java/io/github/levantosina/bankcardmanagement/request/AdminCardRegistrationRequest.java
@@ -3,7 +3,7 @@ package io.github.levantosina.bankcardmanagement.request;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 
-public record CardRegistrationRequest(
+public record AdminCardRegistrationRequest (
         String cardHolderName,
         String encryptedCardNumber,
         YearMonth expiryDate,
