41b0ba35535b39007474021d5565f41846963033
levan <levantosina1992@gmail.com>
2025-05-11 08:55:31 +0300

    add findByBlockRequestedTrueAndCardStatusNot

--- src/main/java/io/github/levantosina/bankcardmanagement/repository/CardRepository.java
+++ src/main/java/io/github/levantosina/bankcardmanagement/repository/CardRepository.java
@@ -1,10 +1,13 @@
 package io.github.levantosina.bankcardmanagement.repository;
 
 import io.github.levantosina.bankcardmanagement.model.CardEntity;
+import io.github.levantosina.bankcardmanagement.model.CardStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 
 
 public interface CardRepository extends JpaRepository<CardEntity, Long> {
@@ -12,5 +15,7 @@ public interface CardRepository extends JpaRepository<CardEntity, Long> {
     boolean existsCardByCardId(Long id);
     Page<CardEntity> findByUserUserId(Long userId, Pageable pageable);
     Page<CardEntity> findByUserUserIdAndCardHolderNameContaining(Long userId, String cardHolderName, Pageable pageable);
+    @Query("SELECT c FROM CardEntity c WHERE c.blockRequest = true AND c.cardStatus <> 'BLOCKED'")
+    List<CardEntity> findByBlockRequestedTrueAndCardStatusNot(CardStatus status);
 
 }
